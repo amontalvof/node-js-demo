d71a1fec28aa6f21a3762d003e717ae18fb6e7e2
amontalvof <amontalvof92@gmail.com>
2023-02-13 10:18:10 -0600

    feat: Module Exports

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-require('./src/add');
+const addFn = require('./src/add');
 
 console.log('Hello World');
+
+const sum = addFn(1, 2);
+const sum2 = addFn(2, 3);
+console.log(sum);
+console.log(sum2);
