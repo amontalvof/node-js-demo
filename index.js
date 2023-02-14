1ef7856130fafee838f41403c69074a76b9ce675
amontalvof <amontalvof92@gmail.com>
2023-02-14 01:08:46 -0600

    feat: Module Scope

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-const addFn = require('./src/add');
-
-console.log('Hello World');
-
-const sum = addFn(1, 2);
-const sum2 = addFn(2, 3);
-console.log(sum);
-console.log(sum2);
+require('./src/batman');
+require('./src/superman');
