c6550740b1cd9e1cc0b65f5398557985ad3148ac
amontalvof <amontalvof92@gmail.com>
2023-02-16 12:54:47 -0600

    feat: Module Caching

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-require('./src/batman');
-require('./src/superman');
+const superHero = require('./src/super-hero');
+const newSuperHero = require('./src/super-hero');
+
+console.log(superHero.getName());
+superHero.setName('Superman');
+console.log(superHero.getName());
+console.log(newSuperHero.getName());
