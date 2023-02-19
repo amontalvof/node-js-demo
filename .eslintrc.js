8702b226247c0faa870f9b7b2f73371f77f5f86e
amontalvof <amontalvof92@gmail.com>
2023-02-19 15:32:25 -0600

    feat: ES Modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    extends: 'eslint:recommended',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        allowImportExportEverywhere: true,
     },
-    "rules": {
-    }
-}
+    rules: {},
+};
---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { add, subtract } from './src/math-esm.mjs';
+
+// import * as math from './src/math-esm.mjs';
+// const { add, subtract } = math;
+
+console.log(add(5, 5));
+console.log(subtract(5, 5));
