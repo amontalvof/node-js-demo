54d9048102fe4c0ed4690a60e53c4b547110f480
amontalvof <amontalvof92@gmail.com>
2023-02-28 10:17:03 -0600

    feat: fs Module

---
+++ src/modules/built-in/fs-module.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const filePath = path.join(__dirname, '../../../file.txt');
+
+console.log('First');
+
+const fileContent = fs.readFileSync(filePath, 'utf-8');
+console.log(fileContent);
+
+console.log('Second');
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+});
+
+console.log('Third');
+
+fs.writeFileSync(filePath, '\nHello World!', { flag: 'a' }); //! flag: 'a' means append
+
+fs.writeFile(filePath, '\nTest', { flag: 'a' }, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('File written');
+    }
+});
