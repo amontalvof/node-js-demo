80fd7c077fce08e0c3d25bfbbc2a8a8e4073647a
amontalvof <amontalvof92@gmail.com>
2023-03-06 10:39:12 -0600

    feat: HTML Response

--- src/modules/built-in/http-module-html.js
+++ src/modules/built-in/http-module-html.js
@@ -1,10 +1,13 @@
+const path = require('node:path');
 const http = require('node:http');
 const fs = require('node:fs');
 
+const htmlPath = path.join(__dirname, '../../../index.html');
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream('index.html').pipe(res);
-    // const html = fs.readFileSync('index.html', 'utf8');
+    fs.createReadStream(htmlPath).pipe(res);
+    // const html = fs.readFileSync(htmlPath, 'utf8');
     // res.end(html);
 });
 
