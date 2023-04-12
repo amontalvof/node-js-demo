1819298cb1f66cd54dffe1d67558b2c0928fe8ae
amontalvof <amontalvof92@gmail.com>
2023-04-12 00:17:41 -0400

    feat: Cluster Module

---
+++ src/modules/built-in/cluster/cluster-module.js
@@ -0,0 +1,23 @@
+const cluster = require('node:cluster');
+const http = require('node:http');
+const OS = require('node:os');
+
+if (cluster.isMaster) {
+    console.log('CPUs: ', OS.cpus().length);
+    console.log(`Master pid: ${process.pid} is running`);
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log(`Worker pid: ${process.pid} started`);
+    const server = http.createServer((req, res) => {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Home page');
+        } else if (req.url === '/slow-page') {
+            for (let i = 0; i < 6000000000; i++) {}
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Slow page');
+        }
+    });
+    server.listen(8000, () => console.log('Server is running on port 8000'));
+}
